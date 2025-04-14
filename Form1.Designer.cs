df8317f8a16463accda4d7ddd9e21b50be428b84
Alexander Blohme <alexander@thrino.no>
2025-04-15 00:31:52 +0200

    ui size

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -62,9 +62,10 @@
             statusStrip1.ImageScalingSize = new Size(32, 32);
             statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, progressBar });
             statusStrip1.LayoutStyle = ToolStripLayoutStyle.HorizontalStackWithOverflow;
-            statusStrip1.Location = new Point(0, 486);
+            statusStrip1.Location = new Point(0, 1050);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(814, 26);
+            statusStrip1.Padding = new Padding(2, 0, 26, 0);
+            statusStrip1.Size = new Size(1512, 42);
             statusStrip1.SizingGrip = false;
             statusStrip1.TabIndex = 11;
             statusStrip1.Text = "statusStrip1";
@@ -72,8 +73,8 @@
             // toolStripStatusLabel1
             // 
             toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            toolStripStatusLabel1.Size = new Size(154, 21);
-            toolStripStatusLabel1.Text = "KCS-Retro v1.0 for Windows";
+            toolStripStatusLabel1.Size = new Size(734, 32);
+            toolStripStatusLabel1.Text = "KCS-Retro v1.0 for Windows - Copyright (c) 2025 Alexander Blohmé";
             // 
             // progressBar
             // 
@@ -83,15 +84,16 @@
             progressBar.Name = "progressBar";
             progressBar.Overflow = ToolStripItemOverflow.Never;
             progressBar.RightToLeftLayout = true;
-            progressBar.Size = new Size(300, 20);
+            progressBar.Size = new Size(557, 36);
             // 
             // fileList
             // 
             fileList.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             fileList.BorderStyle = BorderStyle.FixedSingle;
-            fileList.Location = new Point(12, 49);
+            fileList.Location = new Point(22, 105);
+            fileList.Margin = new Padding(6);
             fileList.Name = "fileList";
-            fileList.Size = new Size(792, 389);
+            fileList.Size = new Size(1469, 828);
             fileList.TabIndex = 20;
             fileList.UseCompatibleStateImageBehavior = false;
             // 
@@ -99,9 +101,10 @@
             // 
             recordFilenames.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             recordFilenames.AutoSize = true;
-            recordFilenames.Location = new Point(236, 445);
+            recordFilenames.Location = new Point(414, 954);
+            recordFilenames.Margin = new Padding(6);
             recordFilenames.Name = "recordFilenames";
-            recordFilenames.Size = new Size(186, 19);
+            recordFilenames.Size = new Size(369, 36);
             recordFilenames.TabIndex = 19;
             recordFilenames.Text = "Record filenames to first block";
             recordFilenames.UseVisualStyleBackColor = true;
@@ -109,9 +112,10 @@
             // removeFileBtn
             // 
             removeFileBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            removeFileBtn.Location = new Point(109, 446);
+            removeFileBtn.Location = new Point(202, 951);
+            removeFileBtn.Margin = new Padding(6);
             removeFileBtn.Name = "removeFileBtn";
-            removeFileBtn.Size = new Size(91, 37);
+            removeFileBtn.Size = new Size(169, 79);
             removeFileBtn.TabIndex = 18;
             removeFileBtn.Text = "&Remove File";
             removeFileBtn.UseVisualStyleBackColor = true;
@@ -120,9 +124,10 @@
             // addFileBtn
             // 
             addFileBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            addFileBtn.Location = new Point(12, 446);
+            addFileBtn.Location = new Point(22, 951);
+            addFileBtn.Margin = new Padding(6);
             addFileBtn.Name = "addFileBtn";
-            addFileBtn.Size = new Size(91, 37);
+            addFileBtn.Size = new Size(169, 79);
             addFileBtn.TabIndex = 17;
             addFileBtn.Text = "&Add File";
             addFileBtn.UseVisualStyleBackColor = true;
@@ -131,9 +136,10 @@
             // recordToFileBtn
             // 
             recordToFileBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            recordToFileBtn.Location = new Point(619, 445);
+            recordToFileBtn.Location = new Point(1150, 949);
+            recordToFileBtn.Margin = new Padding(6);
             recordToFileBtn.Name = "recordToFileBtn";
-            recordToFileBtn.Size = new Size(185, 38);
+            recordToFileBtn.Size = new Size(344, 81);
             recordToFileBtn.TabIndex = 16;
             recordToFileBtn.Text = "Record to wav file";
             recordToFileBtn.UseVisualStyleBackColor = true;
@@ -142,9 +148,10 @@
             // recordToOutputBtn
             // 
             recordToOutputBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            recordToOutputBtn.Location = new Point(428, 445);
+            recordToOutputBtn.Location = new Point(795, 949);
+            recordToOutputBtn.Margin = new Padding(6);
             recordToOutputBtn.Name = "recordToOutputBtn";
-            recordToOutputBtn.Size = new Size(185, 38);
+            recordToOutputBtn.Size = new Size(344, 81);
             recordToOutputBtn.TabIndex = 15;
             recordToOutputBtn.Text = "Record to output";
             recordToOutputBtn.UseVisualStyleBackColor = true;
@@ -155,21 +162,22 @@
             recordInOneFile.AutoSize = true;
             recordInOneFile.Checked = true;
             recordInOneFile.CheckState = CheckState.Checked;
-            recordInOneFile.Location = new Point(237, 464);
+            recordInOneFile.Location = new Point(418, 995);
+            recordInOneFile.Margin = new Padding(6);
             recordInOneFile.Name = "recordInOneFile";
-            recordInOneFile.Size = new Size(144, 19);
+            recordInOneFile.Size = new Size(289, 36);
             recordInOneFile.TabIndex = 21;
             recordInOneFile.Text = "Record all files to 1 file";
             recordInOneFile.UseVisualStyleBackColor = true;
             // 
             // toolStrip1
             // 
-            toolStrip1.ImageScalingSize = new Size(28, 28);
+            toolStrip1.ImageScalingSize = new Size(50, 50);
             toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton8, toolStripButton7, toolStripSeparator3, toolStripButton2, toolStripButton3, toolStripSeparator1, toolStripButton1, toolStripButton4, toolStripSeparator2, toolStripButton5, toolStripButton6 });
             toolStrip1.Location = new Point(0, 0);
             toolStrip1.Name = "toolStrip1";
-            toolStrip1.Padding = new Padding(0, 0, 2, 0);
-            toolStrip1.Size = new Size(814, 35);
+            toolStrip1.Padding = new Padding(0, 0, 4, 0);
+            toolStrip1.Size = new Size(1512, 60);
             toolStrip1.Stretch = true;
             toolStrip1.TabIndex = 22;
             toolStrip1.Text = "toolStrip1";
@@ -180,7 +188,7 @@
             toolStripButton8.Image = (Image)resources.GetObject("toolStripButton8.Image");
             toolStripButton8.ImageTransparentColor = Color.Magenta;
             toolStripButton8.Name = "toolStripButton8";
-            toolStripButton8.Size = new Size(32, 32);
+            toolStripButton8.Size = new Size(54, 54);
             toolStripButton8.Text = "Add files to record";
             toolStripButton8.Click += button5_Click_1;
             // 
@@ -190,7 +198,7 @@
             toolStripButton7.Image = (Image)resources.GetObject("toolStripButton7.Image");
             toolStripButton7.ImageTransparentColor = Color.Magenta;
             toolStripButton7.Name = "toolStripButton7";
-            toolStripButton7.Size = new Size(32, 32);
+            toolStripButton7.Size = new Size(54, 54);
             toolStripButton7.Text = "Remove selected files from list";
             toolStripButton7.Click += button6_Click;
             // 
@@ -198,7 +206,7 @@
             // 
             toolStripSeparator3.Margin = new Padding(5, 0, 5, 0);
             toolStripSeparator3.Name = "toolStripSeparator3";
-            toolStripSeparator3.Size = new Size(6, 35);
+            toolStripSeparator3.Size = new Size(6, 60);
             // 
             // toolStripButton2
             // 
@@ -206,7 +214,7 @@
             toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
             toolStripButton2.ImageTransparentColor = Color.Magenta;
             toolStripButton2.Name = "toolStripButton2";
-            toolStripButton2.Size = new Size(32, 32);
+            toolStripButton2.Size = new Size(54, 54);
             toolStripButton2.Text = "Load from an input source";
             toolStripButton2.Click += toolStripButton2_Click;
             // 
@@ -216,14 +224,14 @@
             toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
             toolStripButton3.ImageTransparentColor = Color.Magenta;
             toolStripButton3.Name = "toolStripButton3";
-            toolStripButton3.Size = new Size(32, 32);
+            toolStripButton3.Size = new Size(54, 54);
             toolStripButton3.Text = "Record to an output device";
             // 
             // toolStripSeparator1
             // 
             toolStripSeparator1.Margin = new Padding(5, 0, 5, 0);
             toolStripSeparator1.Name = "toolStripSeparator1";
-            toolStripSeparator1.Size = new Size(6, 35);
+            toolStripSeparator1.Size = new Size(6, 60);
             // 
             // toolStripButton1
             // 
@@ -231,7 +239,7 @@
             toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
             toolStripButton1.ImageTransparentColor = Color.Magenta;
             toolStripButton1.Name = "toolStripButton1";
-            toolStripButton1.Size = new Size(32, 32);
+            toolStripButton1.Size = new Size(54, 54);
             toolStripButton1.Text = "Load from audio file";
             toolStripButton1.Click += fromWAVFileToolStripMenuItem_Click;
             // 
@@ -241,7 +249,7 @@
             toolStripButton4.Image = (Image)resources.GetObject("toolStripButton4.Image");
             toolStripButton4.ImageTransparentColor = Color.Magenta;
             toolStripButton4.Name = "toolStripButton4";
-            toolStripButton4.Size = new Size(32, 32);
+            toolStripButton4.Size = new Size(54, 54);
             toolStripButton4.Text = "Record to audio file";
             toolStripButton4.Click += recordToFileBtn_Click;
             // 
@@ -249,7 +257,7 @@
             // 
             toolStripSeparator2.Margin = new Padding(5, 0, 5, 0);
             toolStripSeparator2.Name = "toolStripSeparator2";
-            toolStripSeparator2.Size = new Size(6, 35);
+            toolStripSeparator2.Size = new Size(6, 60);
             // 
             // toolStripButton5
             // 
@@ -257,7 +265,7 @@
             toolStripButton5.Image = (Image)resources.GetObject("toolStripButton5.Image");
             toolStripButton5.ImageTransparentColor = Color.Magenta;
             toolStripButton5.Name = "toolStripButton5";
-            toolStripButton5.Size = new Size(32, 32);
+            toolStripButton5.Size = new Size(54, 54);
             toolStripButton5.Text = "Settings";
             toolStripButton5.Click += settingsToolStripMenuItem_Click;
             // 
@@ -267,16 +275,16 @@
             toolStripButton6.Image = (Image)resources.GetObject("toolStripButton6.Image");
             toolStripButton6.ImageTransparentColor = Color.Magenta;
             toolStripButton6.Name = "toolStripButton6";
-            toolStripButton6.Size = new Size(32, 32);
+            toolStripButton6.Size = new Size(54, 54);
             toolStripButton6.Text = "Quit";
             toolStripButton6.Click += toolStripButton6_Click;
             // 
             // mainFrm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlLight;
-            ClientSize = new Size(814, 512);
+            ClientSize = new Size(1512, 1092);
             Controls.Add(toolStrip1);
             Controls.Add(recordInOneFile);
             Controls.Add(fileList);
@@ -286,6 +294,7 @@
             Controls.Add(recordToFileBtn);
             Controls.Add(recordToOutputBtn);
             Controls.Add(statusStrip1);
+            Margin = new Padding(6);
             Name = "mainFrm";
             Text = "KCS-Retro";
             Load += Form1_Load;
